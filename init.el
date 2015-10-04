0dd512d1259c8a4ae05bbeb80fb858bec6240407
Anton Yabchinskiy <arn@bestmx.ru>
2015-10-04 03:02:11 +0300

    Require evil-numbers, bind C +/- to incr/decr a number at point

--- init.el
+++ init.el
@@ -88,6 +88,11 @@
 
 (evil-mode 1)
 
+(require 'evil-numbers)
+
+(global-set-key (kbd "C-c +") 'evil-numbers/inc-at-pt)
+(global-set-key (kbd "C-c -") 'evil-numbers/dec-at-pt)
+
 ;; Ido and Smex
 
 (require 'ido)
