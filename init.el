6adc1c3b96075f73de630c1a8df1fae6cc4ab321
Anton Yabchinskiy <arn@bestmx.net>
2016-08-05 13:56:33 +0300

    use-package for smex, don't bind execute-extended-command

--- init.el
+++ init.el
@@ -41,7 +41,6 @@
                go-mode
                jabber
                rust-mode
-               smex
                toml-mode
                tuareg
                yasnippet))
@@ -108,11 +107,11 @@
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 
-(require 'smex)
-
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+(use-package smex
+  :bind
+  ("M-x" . smex)
+  ("M-X" . smex-major-mode-commands)
+  )
 
 ;; Buffers
 
