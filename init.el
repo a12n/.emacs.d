855250941c7f2f218a808859a3473a9ff862224c
Anton Yabchinskiy <arn@bestmx.net>
2019-02-18 12:01:41 +0300

    Enable melpa and melpa-stable packages

--- init.el
+++ init.el
@@ -24,8 +24,9 @@
 
 (require 'package)
 
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(setq package-archive-priorities '(("melpa" . -10)))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
