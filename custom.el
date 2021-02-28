38bd8d16a27726589361e5389e362e49186ffe8b
Anton Yabchinskiy <arn@bestmx.net>
2021-03-01 00:29:41 +0300

    Update custom file

--- custom.el
+++ custom.el
@@ -4,14 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("2b9dc43b786e36f68a9fd4b36dd050509a0e32fe3b0a803310661edb7402b8b6" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" default)))
+   '("2b9dc43b786e36f68a9fd4b36dd050509a0e32fe3b0a803310661edb7402b8b6" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" default))
  '(evil-auto-indent nil)
- '(frame-background-mode (quote dark))
  '(indent-tabs-mode nil)
  '(package-selected-packages
-   (quote
-    (gruvbox-theme shrink-whitespace elixir-mode yasnippet-snippets yaml-mode use-package tuareg tabbar smex session rust-mode pod-mode muttrc-mode mutt-alias markdown-mode initsplit htmlize graphviz-dot-mode go-mode folding flycheck-rtags evil eproject diminish csv-mode company-rtags browse-kill-ring boxquote bm bar-cursor avy apache-mode)))
+   '(ocp-indent merlin dune tuareg yaml-mode markdown-mode indent-tools aggressive-indent avy yasnippet smex use-package gruvbox-theme evil))
  '(require-final-newline t)
  '(tab-width 4))
 (custom-set-faces
