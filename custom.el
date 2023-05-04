0d15752ce3ec3a51a41dda9610063611565dcd42
Anton Yabchinskiy <arn@bestmx.net>
2023-05-04 23:56:02 +0300

    Update custom.el

--- custom.el
+++ custom.el
@@ -4,12 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("2b9dc43b786e36f68a9fd4b36dd050509a0e32fe3b0a803310661edb7402b8b6" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" default))
+   '("72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "2b9dc43b786e36f68a9fd4b36dd050509a0e32fe3b0a803310661edb7402b8b6" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" default))
  '(evil-auto-indent nil)
  '(evil-intercept-maps '((edebug-mode-map) (ggtags-mode-map)))
  '(indent-tabs-mode nil)
  '(package-selected-packages
-   '(ocp-indent merlin dune tuareg yaml-mode markdown-mode indent-tools aggressive-indent avy yasnippet smex use-package gruvbox-theme evil))
+   '(company ocp-indent merlin dune tuareg yaml-mode markdown-mode indent-tools aggressive-indent avy yasnippet smex use-package gruvbox-theme evil))
  '(require-final-newline t)
  '(tab-width 4))
 (custom-set-faces
