99540e7dc4193e945c06abb3472519654c806a42
Anton Yabchinskiy <arn@bestmx.net>
2023-05-04 23:50:06 +0300

    Adjust evil-intercept-maps in custom file

--- custom.el
+++ custom.el
@@ -6,6 +6,7 @@
  '(custom-safe-themes
    '("2b9dc43b786e36f68a9fd4b36dd050509a0e32fe3b0a803310661edb7402b8b6" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" default))
  '(evil-auto-indent nil)
+ '(evil-intercept-maps '((edebug-mode-map) (ggtags-mode-map)))
  '(indent-tabs-mode nil)
  '(package-selected-packages
    '(ocp-indent merlin dune tuareg yaml-mode markdown-mode indent-tools aggressive-indent avy yasnippet smex use-package gruvbox-theme evil))
